045a584aee89226116017e8654e5e88ded269bbb
Vyron Vasileiadis <hi@fedonman.com>
2026-07-02 19:15:05 +0300

    Revert "init"
    This reverts commit e7f7c5eb3156d2b910bdec3991ab1f3dd0948fdc.

--- qprogram/src/qprogram/operations/reset_phase.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from qprogram.operations.operation import Operation
-
-
-class ResetPhase(Operation):
-    def __init__(self, bus: str) -> None:
-        self.bus = bus
